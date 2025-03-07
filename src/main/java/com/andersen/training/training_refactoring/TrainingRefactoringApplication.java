d27ac39bdbad4c7f4af2fc7dda6bbb47649f78f2
Yaroslav A. Dmitriev <y.dmitriev@andersenlab.com>
2025-03-07 10:51:46 +0300

    Initial

---
+++ src/main/java/com/andersen/training/training_refactoring/TrainingRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.andersen.training.training_refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingRefactoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andersen/training/training_refactoring/TrainingRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersen.training.training_refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingRefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
